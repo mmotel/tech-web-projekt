dcce075c643ee721b99bf342cbfc8b9f2e11ceaf
Mateusz Motel <mateuszmotel@gmail.com>
2013-04-23 22:33:34 +0200

    likwidacja redundancji kodu w skrypt.js

--- public/js/skrypt.js
+++ public/js/skrypt.js
@@ -10,16 +10,6 @@ $(function () {
     socket.on('connect', function (data) {
         console.log('Połączony!');
     });
-	//funckja wypełniająca zawodnika danymi
-	var drawPlayer = function(data){
-		return '<div class="teamMember" id="' + data.id +'">' +
-						'<span class="playerNum">' + data.number + '</span>' +
-						'<span class="playerName">' + data.name + '</span>' +
-						'<span class="countButton"><span class="plus">' + data.plusCount +' </span></span>' +
-						'<span class="countButton"><span class="minus">' + data.minusCount + '</span></span>' +
-					'</div>';
-	};
-	//dane aplikacji
 	
     socket.on('homeTeamData', function (data) {
     	myGUI.drawTeam('home', data);
